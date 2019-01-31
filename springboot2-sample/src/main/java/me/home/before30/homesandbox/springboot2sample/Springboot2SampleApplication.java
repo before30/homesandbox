c255afc7b2e485077632e6a18d0cede776dd001b
Joel <before30@coupang.com>
2019-01-31 11:18:13 +0900

    Initial commit

---
+++ springboot2-sample/src/main/java/me/home/before30/homesandbox/springboot2sample/Springboot2SampleApplication.java
@@ -0,0 +1,14 @@
+package me.home.before30.homesandbox.springboot2sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2SampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot2SampleApplication.class, args);
+	}
+
+}
+
