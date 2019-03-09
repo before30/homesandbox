e9bc69715d230cbd215c72be0d2bf9f47d49a490
Joseph Yang <before30@gmail.com>
2019-03-09 22:41:58 +0900

    Add Baseball game

--- algorithm-ex/src/main/java/cc/before30/algex/general/BaseballGame.java
+++ algorithm-ex/src/main/java/cc/before30/algex/general/BaseballGame.java
@@ -2,8 +2,40 @@ package cc.before30.algex.general;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class BaseballGame {
+    public int calPoints2(String[] ops) {
+        Stack<Integer> stack = new Stack<>();
+
+        for (String op : ops) {
+            switch (op) {
+                case "+":
+                    int top = stack.pop();
+                    int newtop = top + stack.peek();
+                    stack.push(top);
+                    stack.push(newtop);
+                    break;
+                case "C":
+                    stack.pop();
+                    break;
+                case "D":
+                    stack.push(2 * stack.peek());
+                    break;
+                default:
+                    stack.push(Integer.valueOf(op));
+                    break;
+            }
+        }
+        int ans = 0;
+        for (int score : stack) {
+            ans += score;
+        }
+
+        return ans;
+    }
+
+
     public int calPoints(String[] ops) {
 
         List<Integer> list = new ArrayList<>();
@@ -30,6 +62,12 @@ public class BaseballGame {
             }
         }
 
-        return list.stream().reduce(0, (o1, o2) -> o1 + o2);
+
+        int ans = 0;
+        for (int score : list) {
+            ans += score;
+        }
+
+        return ans;
     }
 }
