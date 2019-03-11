a7212e2a460c78c650d94fc4a1438fcd97cd3659
Joseph Yang <before30@gmail.com>
2019-03-11 23:44:43 +0900

    Add Search Binary Search Tree

---
+++ algorithm-ex/src/main/java/cc/before30/algex/tree/SearchBinarySearchTree.java
@@ -0,0 +1,26 @@
+package cc.before30.algex.tree;
+
+public class SearchBinarySearchTree {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val > val) {
+            return searchBST(root.left, val);
+        }
+
+        if (root.val < val) {
+            return searchBST(root.right, val);
+        }
+
+        return root;
+    }
+}
