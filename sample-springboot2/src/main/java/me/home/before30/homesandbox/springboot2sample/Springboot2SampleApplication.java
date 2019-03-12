ff4e6a49ecb4be5524c3e8e373a32e0c93770077
Joseph Yang <before30@gmail.com>
2019-03-12 23:21:42 +0900

    Add Polish

--- sample-springboot2/src/main/java/me/home/before30/homesandbox/springboot2sample/Springboot2SampleApplication.java
+++ sample-springboot2/src/main/java/me/home/before30/homesandbox/springboot2sample/Springboot2SampleApplication.java
@@ -1,14 +1,31 @@
 package me.home.before30.homesandbox.springboot2sample;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class Springboot2SampleApplication {
+@Slf4j
+@RestController
+//@EnableAutoConfiguration
+public class Springboot2SampleApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot2SampleApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Hello World");
+	}
+
+	@RequestMapping("/")
+	String home() {
+		return "Hello World!";
+	}
 }
 
