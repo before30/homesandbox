e6df74e4f42e5f74df8a79f28bd0b6da2c47bf14
Joseph Yang <before30@gmail.com>
2019-03-12 23:37:18 +0900

    Add TwoSum

---
+++ algorithm-ex/src/main/java/cc/before30/algex/general/TwoSum.java
@@ -0,0 +1,38 @@
+package cc.before30.algex.general;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
+    }
+
+    public int[] twoSumII(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            int x = target - nums[i];
+            if (map.containsKey(x) && map.get(x) != i) {
+                return new int[]{i, map.get(x)};
+            }
+        }
+
+        return null;
+    }
+
+    public int[] twoSumIII(int[] nums, int target) {
+        return null;
+    }
+}
