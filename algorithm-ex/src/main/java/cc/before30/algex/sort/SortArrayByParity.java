df8fb42954b777ab9ee3ac197340bb9f7c2919cf
Joseph Yang <before30@gmail.com>
2019-03-14 23:47:43 +0900

    Add SortArray By Parity

--- algorithm-ex/src/main/java/cc/before30/algex/sort/SortArrayByParity.java
+++ algorithm-ex/src/main/java/cc/before30/algex/sort/SortArrayByParity.java
@@ -1,8 +1,14 @@
 package cc.before30.algex.sort;
 
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class SortArrayByParity {
+
+    public int[] sortArrayByParity2(int[] A) {
+        return Arrays.stream(A).boxed().sorted((x,y) -> Integer.compare(x%2, y%2)).mapToInt(i -> i)
+                .toArray();
+    }
 
     public int[] sortArrayByParity(int[] A) {
         int[] even = Arrays.stream(A).filter(x -> x % 2 == 0).toArray();
