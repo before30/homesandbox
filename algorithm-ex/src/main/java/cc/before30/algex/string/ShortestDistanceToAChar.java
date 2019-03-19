4a42c5b4657ab6f92bb145fe413626f69c724507
Joseph Yang <before30@gmail.com>
2019-03-20 00:20:36 +0900

    Add Shortest Distance to a char

--- algorithm-ex/src/main/java/cc/before30/algex/string/ShortestDistanceToAChar.java
+++ algorithm-ex/src/main/java/cc/before30/algex/string/ShortestDistanceToAChar.java
@@ -1,6 +1,30 @@
 package cc.before30.algex.string;
 
 public class ShortestDistanceToAChar {
+
+    public int[] shortestToCharII(String s, char c) {
+        int n = s.length();
+        int[] ans = new int[n];
+        int prev = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i) == c) {
+                prev = i;
+            }
+            ans[i] = i - prev;
+        }
+
+        prev = Integer.MAX_VALUE;
+        for (int i = n - 1; i >= 0; i--) {
+            if (s.charAt(i) == c) {
+                prev = i;
+            }
+            ans[i] = Math.min(ans[i], prev - i);
+        }
+
+        return ans;
+    }
+
     public int[] shortestToChar(String S, char C) {
 
         int[] ans = new int[S.length()];
