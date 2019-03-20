f8308ce6fc8ea9295af766309be8a70c22c6abcc
Joseph Yang <before30@gmail.com>
2019-03-21 00:11:00 +0900

    Add binarysearch

--- algorithm-ex/src/main/java/cc/before30/algex/number/SumOfSquareNumbers.java
+++ algorithm-ex/src/main/java/cc/before30/algex/number/SumOfSquareNumbers.java
@@ -23,6 +23,28 @@ public class SumOfSquareNumbers {
     }
 
     public boolean solution3(int c) {
+
+        for (long a = 0; a * a <= c; a++) {
+            int bSquare = c - (int)(a * a);
+            if (binary_search(0, bSquare, bSquare)) {
+                return true;
+            }
+        }
         return false;
+    }
+
+    public boolean binary_search(long s, long e, int n) {
+        if (s > e) {
+            return false;
+        }
+
+        long mid = s + (e - s) / 2;
+        if (mid * mid == n) {
+            return true;
+        }
+        if (mid * mid > n) {
+            return binary_search(s, mid -1, n);
+        }
+        return binary_search(mid+1, e, n);
     }
 }
