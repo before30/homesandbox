9107b8105e9db31d7ac5fc630a147bf7ee598c73
Joseph Yang <before30@gmail.com>
2019-03-21 23:18:52 +0900

    Add Power of two

--- algorithm-ex/src/main/java/cc/before30/algex/number/PowerOfTwo.java
+++ algorithm-ex/src/main/java/cc/before30/algex/number/PowerOfTwo.java
@@ -1,6 +1,18 @@
 package cc.before30.algex.number;
 
 public class PowerOfTwo {
+    public boolean solution1(int n) {
+        if ( n <= 0 ) {
+            return false;
+        }
+
+        while ( n % 2 == 0 ) {
+            n = n / 2;
+        }
+
+        return n == 1;
+    }
+
     public boolean isPowerOfTwo(int n) {
         if (n <= 0) {
             return false;
