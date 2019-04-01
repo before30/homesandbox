820d51089c3f83573d71d395c0941df23e5ebadc
Joseph Yang <before30@gmail.com>
2019-04-01 23:21:53 +0900

    Polish

--- algorithm-ex/src/main/java/cc/before30/algex/number/SumOfNumbersAfterQuery.java
+++ algorithm-ex/src/main/java/cc/before30/algex/number/SumOfNumbersAfterQuery.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class SumOfNumbersAfterQuery {
     public int[] sumEvenAfterQueries(int[] A, int[][] queries) {
-        int sum = Arrays.stream(A).sum();
+        int sum = Arrays.stream(A).filter(i -> i % 2 == 0).sum();
 
         int[] ans = new int[queries.length];
         for (int i = 0; i < queries.length; i++) {
