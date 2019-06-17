296c5e90c4963ba515f9908d06fd93dc01dfb525
Joseph Yang <before30@gmail.com>
2019-06-17 22:38:19 +0900

    Add DP Practice

--- algorithm-ex/src/main/java/cc/before30/algex/dp/ClimbingStairs.java
+++ algorithm-ex/src/main/java/cc/before30/algex/dp/ClimbingStairs.java
@@ -23,19 +23,4 @@ public class ClimbingStairs {
         memo[i] = doClimbStairs(i + 1, n, memo) + doClimbStairs(i + 2, n, memo);
         return memo[i];
     }
-
-    public int climbStairsDp(int n) {
-        if (n == 1) {
-            return 1;
-        }
-
-        int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        for (int i = 3; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
-        }
-
-        return dp[n];
-    }
 }
---
+++ algorithm-ex/src/main/java/cc/before30/algex/dp/ClimbingStairs2.java
@@ -0,0 +1,26 @@
+package cc.before30.algex.dp;
+
+/**
+ * ClimbingStairs2
+ *
+ * @author before30
+ * @since 2019-06-17
+ */
+public class ClimbingStairs2 {
+
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
+    }
+}
